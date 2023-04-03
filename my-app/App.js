3411d7fe8230d35f6010ebdf24c569f976924591
mostafaturk <121161662+mostafaturk@users.noreply.github.com>
2023-04-03 21:05:01 +0200

    Update App.js
     add forget password and welcome screen to the app

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,8 @@
 import HomeScreen from "./Screens/HomeScreen.js";
 import LoginScreen from "./Screens/LoginScreen.js";
 import RegisterScreen from "./Screens/RegisterScreen.js";
+import Welcome from "./Screens/welcome.js";
+import Forgetpassword from "./Screens/forgetpassword.js";
 import ProfileScreen from "./Screens/ProfileScreen.js";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -9,12 +11,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
+    //  <Forgetpassword/>
     <NavigationContainer>
       <Stack.Navigator>
+         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+   <Stack.Screen name="Forgetpassword" component={Forgetpassword} />
       </Stack.Navigator>
     </NavigationContainer>
   );
