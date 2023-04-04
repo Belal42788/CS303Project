ead3f1d51046962dfa17a7401f61b9149f1fdcf7
Mohamed Abdallah <mohaabdallah2002287@gmail.com>
2023-04-04 13:24:22 -0700

    update database

--- my-app/firebase/config/firebase-config.js
+++ my-app/firebase/config/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCDQIa8F4YG_CbPnvjFww-IgY-EtZJjROE",
@@ -8,8 +9,11 @@ const firebaseConfig = {
     storageBucket: "twsela-71a88.appspot.com",
     messagingSenderId: "1084505782046",
     appId: "1:1084505782046:web:d41387c47d488afad5fb0f",
+    databaseURL: "https://twsela-71a88-default-rtdb.firebaseio.com/",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 export default auth;
+export  {database};
