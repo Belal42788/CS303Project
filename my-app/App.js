a0b7c48dd393ce39ed7bac55e3bff6c1fc6cabb7
MoatazEssam <120606709+MoatazEssam@users.noreply.github.com>
2023-04-09 03:04:04 +0200

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,8 +1,11 @@
+import HomeScreen from "./Screens/HomeScreen.js";
 import LoginScreen from "./Screens/LoginScreen.js";
 import RegisterScreen from "./Screens/RegisterScreen.js";
 import Welcome from "./Screens/welcome.js";
 import Forgetpassword from "./Screens/Forgetpassword.js";
 import ProfileScreen from "./Screens/ProfileScreen.js";
+import Getstar1 from "./Screens/GetStar1.js";
+import Getstar2 from "./Screens/GetStar2.js";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -10,9 +13,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
+    //  <Forgetpassword/>
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Getstat1" component={Getstar1} />
+      <Stack.Screen name="Getstat2" component={Getstar2} />
         <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
