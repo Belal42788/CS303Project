2672245a46b5c2b2fda64681858fcc08d8f76fa0
MoatazEssam <120606709+MoatazEssam@users.noreply.github.com>
2023-04-09 08:19:02 +0200

    Update LoginScreen.js

--- my-app/Screens/LoginScreen.js
+++ my-app/Screens/LoginScreen.js
@@ -1,198 +1,195 @@
-import { signInWithEmailAndPassword ,signOut} from "firebase/auth"
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { StatusBar } from "expo-status-bar";
-import auth from '../firebase/config/firebase-config.js'
+import auth from "../firebase/config/firebase-config.js";
 import React, { useState } from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TextInput,
-    TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
-import { getAuth,FacebookAuthProvider,signInWithCredential} from "firebase/auth";
 import {
-    AccessToken,
-    LoginManager,
-  } from 'react-native-fbsdk-next';
-
+  getAuth,
+  FacebookAuthProvider,
+  signInWithCredential,
+} from "firebase/auth";
+import { AccessToken, LoginManager } from "react-native-fbsdk-next";
 
 const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = auth.currentUser;
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const user = auth.currentUser;
-    
+  const HandleSignin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        if (user.emailVerified) {
+          navigation.navigate("Profile");
+        } else {
+          signOut(auth).then(() => alert("Email is not Verified"));
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert("Email or Password is wrong");
+        console.log(errorMessage);
+      });
+  };
 
-    const HandleSignin= () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                if (user.emailVerified) {
-                    navigation.navigate('Profile')
-                } else {
-                    signOut(auth)
-                    .then(()=>alert("Email is not Verified"))
-                }
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                alert("Email or Password is wrong")
-                console.log(errorMessage);
-            });
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../assets/login.png")} />
+      <StatusBar style="auto" />
 
-    }
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Enter your Email here:"
+          placeholderTextColor="#003f5c"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
 
-    return (
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Enter your Password:"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
 
-        <View style={styles.container}>
-            <Image style={styles.image} source={require("../assets/login.png")} />
-            <StatusBar style="auto" />
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.buttonText} onPress={HandleSignin}>
+          LOGIN
+        </Text>
+      </TouchableOpacity>
 
+      <TouchableOpacity style={styles.registerBtn}>
+        <Text
+          style={styles.buttonText}
+          onPress={() => navigation.navigate("Register")}
+        >
+          Register
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text
+          style={styles.forgot_button}
+          onPress={() => navigation.navigate("Forgetpassword")}
+        >
+          Forgot your password?
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.smallloginicon}>
+        <TouchableOpacity >
+        {/* <TouchableOpacity onPress={onFacebookButtonPress}> */}
+          <Image
+            style={styles.smallloginicon}
+            source={require("../assets/thcc.png")}
+          />
+        </TouchableOpacity>
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Enter your Email here:"
-                    placeholderTextColor="#003f5c"
-                    value={email}
-                    onChangeText={setEmail}
-                />
-            </View>
-
-
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Enter your Password:"
-                    placeholderTextColor="#003f5c"
-                    secureTextEntry={true}
-                    value={password}
-                    onChangeText={setPassword}
-                />
-            </View>
-
-
-
-            <TouchableOpacity style={styles.loginBtn}>
-                <Text style={styles.buttonText} onPress={HandleSignin}>LOGIN</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.registerBtn}>
-                <Text style={styles.buttonText} onPress={() => navigation.navigate("Register")}>Register</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Text
-                    style={styles.forgot_button}
-                    onPress={() => navigation.navigate("Forgetpassword")}
-                >
-                    Forgot your password?
-                </Text>
-            </TouchableOpacity>
-            <View style={styles.smallloginicon}>
-                <TouchableOpacity onPress={onFacebookButtonPress} >
-                    <Image
-                        style={styles.smallloginicon}
-                        source={require("../assets/thcc.png")}
-                    />
-                </TouchableOpacity>
-
-                <TouchableOpacity >
-                    <Image
-                        style={styles.smallloginicon}
-                        source={require("../assets/gmail_icon-icons.com_62758.png")}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.smallloginicon}
-                        source={require("../assets/twitter.png")}
-                    />
-                </TouchableOpacity>
-            </View>
-
-        </View>
-    )
-}
+        <TouchableOpacity>
+          <Image
+            style={styles.smallloginicon}
+            source={require("../assets/gmail_icon-icons.com_62758.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={styles.smallloginicon}
+            source={require("../assets/twitter.png")}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    image: {
-        marginBottom: 40,
-        width: 66,
-        height: 66,
-
-    },
-    inputView: {
-        backgroundColor: "white",
-        borderColor: "black",
-        borderWidth: 2.5,
-        borderRadius: 5,
-        width: "85%",
-        height: 40,
-        marginBottom: 20,
-        alignItems: "center",
-        textAlign: "left",
-    },
-    TextInput: {
-        fontSize: 16,
-        width: "100%",
-        height: 50,
-        padding: 5,
-        textAlign: "left",
-        color: "black",
-    },
-    forgot_button: {
-        marginBottom: 40,
-        marginTop: 20,
-        color: "#003f5c",
-        textDecorationLine: "underline",
-        fontSize: 20,
-        color: "rgb(100, 128, 128)",
-    },
-    loginBtn: {
-        width: "70%",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 10,
-        backgroundColor: "royalblue",
-        fontWeight: "bold"
-    },
-    registerBtn: {
-        width: "70%",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 10,
-        backgroundColor: "royalblue",
-        fontWeight: 'bold'
-    },
-    buttonText: {
-        color: "black",
-        fontWeight: "bold",
-        textTransform: "capitalize",
-        fontSize: 15,
-        textAlign: "center",
-    },
-    smallloginicon: {
-        width: 60,
-        height: 60,
-        margin: 5,
-        marginTop: 0,
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignContent: "space-around",
-        borderRadius: "50%",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    marginBottom: 40,
+    width: 66,
+    height: 66,
+  },
+  inputView: {
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 2.5,
+    borderRadius: 5,
+    width: "85%",
+    height: 40,
+    marginBottom: 20,
+    alignItems: "center",
+    textAlign: "left",
+  },
+  TextInput: {
+    fontSize: 16,
+    width: "100%",
+    height: 50,
+    padding: 5,
+    textAlign: "left",
+    color: "black",
+  },
+  forgot_button: {
+    marginBottom: 40,
+    marginTop: 20,
+    color: "#003f5c",
+    textDecorationLine: "underline",
+    fontSize: 20,
+    color: "rgb(100, 128, 128)",
+  },
+  loginBtn: {
+    width: "70%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    backgroundColor: "royalblue",
+    fontWeight: "bold",
+  },
+  registerBtn: {
+    width: "70%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    backgroundColor: "royalblue",
+    fontWeight: "bold",
+  },
+  buttonText: {
+    color: "black",
+    fontWeight: "bold",
+    textTransform: "capitalize",
+    fontSize: 15,
+    textAlign: "center",
+  },
+  smallloginicon: {
+    width: 60,
+    height: 60,
+    margin: 5,
+    marginTop: 0,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignContent: "space-around",
+    borderRadius: "50%",
+  },
 });
 
-export default LoginScreen
+export default LoginScreen;
