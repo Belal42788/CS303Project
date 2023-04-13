070cbe3c850062aee9d3475c2d920266f3a234fc
Muhammed2027199 <muhammed9ragab@gmail.com>
2023-04-13 17:10:32 +0200

    Create Footer.js

---
+++ my-app/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+const Footer = () => {
+  return (
+    <View style={styles.footer}>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>home</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = {
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    width: '100%',
+    height: 60,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    marginHorizontal: 10,
+    backgroundColor: '#007aff',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+};
+
+export default Footer;
