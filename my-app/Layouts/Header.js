22aed662f66097e6f0213e0355456768f13f3bf8
Mohamed Abdallah <mohaabdallah2002287@gmail.com>
2023-04-20 15:37:48 +0200

    some css

--- my-app/Layouts/Header.js
+++ my-app/Layouts/Header.js
@@ -9,25 +9,26 @@ import {
     Alert,
 } from "react-native";
 
-
 function Header({ navigation }) {
     const user = auth.currentUser;
     return (
         <View>
-        <View style={styles.header}>
-            <View style={styles.searchbarview}>
-                <TextInput
-                    style={styles.searchbartext}
-                    placeholder="  🔎 Search "
-                    placeholderTextColor="#003f5c"
-                />
+            <View style={styles.header}>
+                <View style={styles.searchbarview}>
+                    <Image source={require("../assets/Image/Search.png")} style={styles.photoSearch} />
+                    <TextInput
+                        style={styles.searchbartext}
+                        placeholder="Search "
+                        placeholderTextColor="black"
+                        underlineColorAndroid="transparent"
+                    />
+                </View>
+                <View style={styles.photoGroub}>
+                    <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+                        <Image source={user.photoURL} style={styles.PhotoStyle} />
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.photoGroub}>
-                <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
-                    <Image source={user.photoURL} style={styles.PhotoStyle} />
-                </TouchableOpacity>
-            </View>
-        </View>
         </View>
     );
 }
@@ -42,10 +43,11 @@ const styles = StyleSheet.create({
         alignContent: "space-between",
         marginTop: 20,
         marginBottom: 5,
-        marginLeft:"10%",
+        marginLeft: "10%",
         backgroundColor: "rgb(206, 158, 4)",
     },
     searchbarview: {
+        flexDirection: 'row',
         backgroundColor: "white",
         borderColor: "black",
         borderWidth: 2.5,
@@ -63,32 +65,42 @@ const styles = StyleSheet.create({
     },
     searchbartext: {
         fontSize: "80%",
-        width: "90%",
-        height: "90%",
+        width: "80%",
+        height: "85%",
         textAlign: "left",
         color: "black",
         fontFamily: "cairo",
         fontWeight: "700",
-        marginLeft: 12,
-        marginTop: 2,
+        marginLeft: -5,
+        marginTop: 4,
+        borderRadius:"25%",
+        borderWidth:10,
+        borderColor:"white"
     },
     photoGroub: {
         marginTop: -10,
         marginLeft: 10,
         marginBottom: 5,
-        width:54,
-        height:54,
-        backgroundColor:"black",
+        width: 54,
+        height: 54,
+        backgroundColor: "black",
         borderRadius: "50%",
         bordertWidth: 20,
     },
     PhotoStyle: {
-        marginTop:2,
-        marginLeft:2,
+        marginTop: 2,
+        marginLeft: 2,
         width: 50,
         height: 50,
         borderRadius: "50%",
-        borderColor: "black"
+        borderColor: "black",
     },
+    photoSearch:{
+        width: 50,
+        height: 50,
+        borderRadius: "50%",
+        borderColor: "black",
+        marginBottom:10
+    }
 });
 export default Header;
