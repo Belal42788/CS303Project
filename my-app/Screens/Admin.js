84a80c100fb905e872f32f996d707631556d0124
belal42788 <asama4278@gmail.com>
2023-05-08 21:02:41 +0300

    Add Car

--- my-app/Screens/Admin.js
+++ my-app/Screens/Admin.js
@@ -159,7 +159,7 @@ const Admin = ({ navigation }) => {
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText} onPress={() => { navigation.navigate('AddModel') }}>
+                <Text style={styles.buttonText} onPress={() => { navigation.navigate('BlankCar') }}>
                     Add Model
                 </Text>
             </TouchableOpacity>
