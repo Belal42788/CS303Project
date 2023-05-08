2db8f6bdc7203919c0ee0277d2eb8329a0e50f33
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-05-09 02:11:58 +0300

    Update Car.js

--- my-app/Screens/Car.js
+++ my-app/Screens/Car.js
@@ -10,6 +10,7 @@ import {
   TouchableOpacity,
   Alert, ImageBackground, FlatList
 } from "react-native";
+import Footer from '../Layouts/Footer';
 function Car({ navigation, route }) {
 
   return (
@@ -51,6 +52,7 @@ function Car({ navigation, route }) {
       <TouchableOpacity style={styles.button} >
         <Text style={styles.buttonText}>Checkout</Text>
       </TouchableOpacity> 
+      <Footer navigation={navigation} />
     </LinearGradient>
   );
 };
