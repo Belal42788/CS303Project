066a9fd33f6d082e67ad1d5fb2a8525357369b40
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-05-09 15:50:04 +0300

    Revert "edit"
    This reverts commit dcbc8ee4c3338465a2a535b0cf854a106b39e244.

--- my-app/Screens/ProfileScreen.js
+++ my-app/Screens/ProfileScreen.js
@@ -263,7 +263,6 @@ const ProfileScreen = ({ navigation }) => {
 
 
   return (
-
     <LinearGradient style={[styles.container]} colors={["#1c2834", "#d0a20e"]} start={{ x: 0, y: 0.5 }} end={{ x: 1, y: 0.5 }} locations={[0, 0.8]}>
       <StatusBar style="auto" />
       <View style={styles.logocont}>
@@ -284,9 +283,6 @@ const ProfileScreen = ({ navigation }) => {
           <Text style={styles.updatePhotoButtonText}>+</Text>
         </TouchableOpacity>
       </TouchableOpacity>
-
-
-
 
       {
         firstNameMode ?
@@ -332,7 +328,6 @@ const ProfileScreen = ({ navigation }) => {
             </>
           )
       }
-
 
       {
         lastNameMode ?
@@ -513,9 +508,10 @@ const ProfileScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     minHeight:'110vh',
+    height: 'auto',
     alignItems: "center",
     justifyContent: "center",
-    height: 'auto',
+    
     width: '100%',
   },
   logoText: {
@@ -531,7 +527,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     // justifyContent: "flex-start",
     width: "100%",
-    marginBottom: '-100%',
+    // marginBottom: '-5%',
     position: 'absolute',
     top: 0,
   },
@@ -620,7 +616,6 @@ const styles = StyleSheet.create({
     marginLeft: 5,
   },
   editImage: {
-    marginTop:'-70%',
     width: 30,
     height: 30,
     borderRadius: 10,
@@ -643,12 +638,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   photo: {
-    marginTop:'-60%',
     width: 120,
     height: 120,
     borderColor: "#1c2834",
     borderRadius: "50%",
-    marginBottom: 2,
+    marginBottom: 25,
     borderWidth: 3,
   },
   updatePhotoButton: {
